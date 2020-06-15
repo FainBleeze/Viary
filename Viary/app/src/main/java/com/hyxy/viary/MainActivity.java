df12063b93dca84cb50947172448776c6510ad06
FainBleeze <49508652+FainBleeze@users.noreply.github.com>
2020-06-15 14:35:16 +0800

    可运行

--- Viary/app/src/main/java/com/hyxy/viary/MainActivity.java
+++ Viary/app/src/main/java/com/hyxy/viary/MainActivity.java
@@ -7,6 +7,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_main);
     }
 }

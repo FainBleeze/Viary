0f8940d52638e169c46365c64a9ce56d46a5a442
Woodencross <qichong49@163.com>
2020-06-16 12:19:02 +0800

    add some xml

--- Viary/app/src/main/java/com/hyxy/viary/MainActivity.java
+++ Viary/app/src/main/java/com/hyxy/viary/MainActivity.java
@@ -2,8 +2,11 @@ package com.hyxy.viary;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+    private ListView dairyListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

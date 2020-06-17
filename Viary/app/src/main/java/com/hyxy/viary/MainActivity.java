10581c709d2e563e2f6cf2cb9757fee5497b4f99
Woodencross <qichong49@163.com>
2020-06-17 21:53:04 +0800

    实现了主界面数据库查询

--- Viary/app/src/main/java/com/hyxy/viary/MainActivity.java
+++ Viary/app/src/main/java/com/hyxy/viary/MainActivity.java
@@ -3,6 +3,7 @@ package com.hyxy.viary;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.CharArrayBuffer;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
@@ -14,35 +15,68 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class MainActivity extends Activity {
     private ListView diaryListView;
+    private int currentYear;//当前显示的年份
+    private int currentMonth;//当前显示的月份
+    private int currentDay;
     private List<DiaryItem> monthDiaryList=new ArrayList<>();
     Dbo db_helper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main);
         //api高于23时，动态申请权限
         verifyStoragePermissions(MainActivity.this);
+
+        //获取当前年、月以选择显示哪一月的diary
+        Calendar calendar=Calendar.getInstance();
+        currentYear=calendar.get(Calendar.YEAR);
+        currentMonth=calendar.get(Calendar.MONTH)+1;//从0开始，要+1
+        currentDay=calendar.get(Calendar.DAY_OF_MONTH);
+        int dayOfCurrentMonth=calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+        System.out.println("year:"+currentYear+"\nmonth:"+currentMonth+"\nday:"+currentDay+"\nday of month:"+dayOfCurrentMonth);
+
         //打开数据库
         db_helper = new Dbo(MainActivity.this);
         //用下面这个读取数据库，返回到cursor
         SQLiteDatabase db = db_helper.getReadableDatabase();
-        Cursor res;
+        String sql = "select * from "+params.DBTABLENAME+" where "
+                +params.DBYEAR+"=? and "+params.DBMONTH+"=? and "+params.DBDAY+"=?;";
+        //res.moveToFirst();
+        DiaryItem item;
+        for(int dayIndex=1;dayIndex<=currentDay;dayIndex++) {
+            //System.out.println("dayIndex:"+dayIndex);
+            String[] sql_params={Integer.toString(currentYear),Integer.toString(currentMonth),Integer.toString(dayIndex)};
+            Cursor res = db.rawQuery(sql, sql_params);
+            if(res.getCount()==0){
+                item=new DiaryItem(currentYear,currentMonth,dayIndex,null);
+                monthDiaryList.add(item);
+            }
+            else{
+                CharArrayBuffer buf = null;
+                res.copyStringToBuffer(res.getColumnIndex(params.DBCONTENT), buf);
+                System.out.println("day "+dayIndex+"'s buf is:"+buf.toString());
+                item=new DiaryItem(currentYear,currentMonth,dayIndex,buf.toString());
+                monthDiaryList.add(item);
+            }
+            res.close();
+        }
+
+        //测试用实例
+        /*
         monthDiaryList.add(new DiaryItem(2020,6,16,"June sixteen"));
         monthDiaryList.add(new DiaryItem(2020,6,17,null));
         monthDiaryList.add(new DiaryItem(2020,6,18,"June eighteen"));
         monthDiaryList.add(new DiaryItem(2020,6,19,null));
         monthDiaryList.add(new DiaryItem(2020,6,20,null));
-        monthDiaryList.add(new DiaryItem(2020,6,21,"豫章故郡，洪都新府。星分翼轸，地接衡庐。襟三江而带五湖，控蛮荆而引瓯越。物华天宝，龙光射牛斗之墟；人杰地灵，徐孺下陈蕃之榻。雄州雾列，俊采星驰。台隍枕夷夏之交，宾主尽东南之美。都督阎公之雅望，棨戟遥临；宇文新州之懿范，襜帷暂驻。十旬休假，胜友如云；千里逢迎，高朋满座。腾蛟起凤，孟学士之词宗；紫电青霜，王将军之武库。家君作宰，路出名区；童子何知，躬逢胜饯。\n" +
-                "时维九月，序属三秋。潦水尽而寒潭清，烟光凝而暮山紫。俨骖騑于上路，访风景于崇阿；临帝子之长洲，得天人之旧馆。层峦耸翠，上出重霄；飞阁流丹，下临无地。鹤汀凫渚，穷岛屿之萦回；桂殿兰宫，即冈峦之体势。\n" +
-                "披绣闼，俯雕甍，山原旷其盈视，川泽纡其骇瞩。闾阎扑地，钟鸣鼎食之家；舸舰弥津，青雀黄龙之舳。云销雨霁，彩彻区明。落霞与孤鹜齐飞，秋水共长天一色。渔舟唱晚，响穷彭蠡之滨；雁阵惊寒，声断衡阳之浦。\n" +
-                "遥襟甫畅，逸兴遄飞。爽籁发而清风生，纤歌凝而白云遏。睢园绿竹，气凌彭泽之樽；邺水朱华，光照临川之笔。四美具，二难并。穷睇眄于中天，极娱游于暇日。天高地迥，觉宇宙之无穷；兴尽悲来，识盈虚之有数。望长安于日下，目吴会于云间。地势极而南溟深，天柱高而北辰远。关山难越，谁悲失路之人？萍水相逢，尽是他乡之客。怀帝阍而不见，奉宣室以何年？\n" +
-                "嗟乎！时运不齐，命途多舛。冯唐易老，李广难封。屈贾谊于长沙，非无圣主；窜梁鸿于海曲，岂乏明时？所赖君子见机，达人知命。老当益壮，宁移白首之心？穷且益坚，不坠青云之志。酌贪泉而觉爽，处涸辙以犹欢。北海虽赊，扶摇可接；东隅已逝，桑榆非晚。孟尝高洁，空余报国之情；阮籍猖狂，岂效穷途之哭！\n" +
-                "勃，三尺微命，一介书生。无路请缨，等终军之弱冠；有怀投笔，慕宗悫之长风。舍簪笏于百龄，奉晨昏于万里。非谢家之宝树，接孟氏之芳邻。他日趋庭，叨陪鲤对；今兹捧袂，喜托龙门。杨意不逢，抚凌云而自惜；钟期既遇，奏流水以何惭？\n" +
-                "呜乎！胜地不常，盛筵难再；兰亭已矣，梓泽丘墟。临别赠言，幸承恩于伟饯；登高作赋，是所望于群公。敢竭鄙怀，恭疏短引；一言均赋，四韵俱成。请洒潘江，各倾陆海云尔"));
+        monthDiaryList.add(new DiaryItem(2020,6,21,null));
+        */
+
         DiaryAdapter adapter=new DiaryAdapter(MainActivity.this,monthDiaryList);
         adapter.notifyDataSetChanged();
         diaryListView=(ListView)findViewById(R.id.diary_listview);
@@ -57,7 +91,13 @@ public class MainActivity extends Activity {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             DiaryItem d=monthDiaryList.get(position);
-            Intent intent=new Intent(MainActivity.this,ChooseActivity.class);
+            Intent intent;
+            if(d.getDiaryContent()==null){
+                intent=new Intent(MainActivity.this,ChooseActivity.class);
+            }
+            else{
+                intent=new Intent(MainActivity.this,DiaryActivity.class);
+            }
             Bundle bundle=new Bundle();
             bundle.putInt(params.YearKey,d.getYear());
             bundle.putInt(params.MonthKey,d.getMonth());
@@ -79,7 +119,7 @@ public class MainActivity extends Activity {
 
             default:
         }
-        startActivity(new Intent(MainActivity.this, ChooseActivity.class));
+        //startActivity(new Intent(MainActivity.this, ChooseActivity.class));
     }
 
     //读写权限列表
